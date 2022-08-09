b137fd5ba338b5747e9a012f6418a239d29fbd24
mitreska <estudio.mtp@gmail.com>
2022-08-09 18:00:56 -0300

    pod install
    creating podfile and xcode workspace

---
+++ Pods/Target Support Files/Pods-Pokemon Test App-Pokemon Test AppUITests/Pods-Pokemon Test App-Pokemon Test AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pokemon_Test_App_Pokemon_Test_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Pokemon_Test_App_Pokemon_Test_AppUITests
+@end
